fc60421c82ee52ae29216bbc577740a8cefd8d9a
Victor Belinello <victorbelinello17@gmail.com>
2021-04-26 21:27:00 -0300

    sla

--- artag_follower/src/follower.py
+++ artag_follower/src/follower.py
@@ -4,28 +4,26 @@ from ar_track_alvar_msgs.msg import AlvarMarkers
 from geometry_msgs.msg import Point
 
 target_id = 0
+pub = rospy.Publisher('/govai', Point, queue_size=10)
+
 def update_goal(markers_data):
     for m in markers_data.markers:
         if m.id == target_id:
-            
-            rospy.loginfo(m.pose.pose.position.z)
+            pt = Point()
+            pt.x = m.pose.pose.position.z - 0.8
+            pt.y = -1 * m.pose.pose.position.x
+            rospy.loginfo(pt)
+            if pt.x > 0:
+                pub.publish(pt)
             
 def get_artag_pose():
-    pub = rospy.Publisher('/govai', Point, queue_size=10)
     rospy.init_node('follower', anonymous=True)
-    # rospy.Subscriber('/ar_pose_marker', AlvarMarkers, update_goal)
+    rospy.Subscriber('/ar_pose_marker', AlvarMarkers, update_goal)
     rospy.loginfo("No iniciado")
-    
-    pt = Point()
-    pt.x = 7
-    pt.y = 1
-    rospy.loginfo(pt)
-    pub.publish(pt)
-    
-    rospy.loginfo("Publicando")
 
     rospy.spin()
 if __name__ == "__main__":
+    
     try:
         get_artag_pose()
     except rospy.ROSInterruptException:
--- move_base_yolo/src/move_base.py
+++ move_base_yolo/src/move_base.py
@@ -4,48 +4,43 @@
 import rospy
 import actionlib
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
+from nav_msgs.msg import Odometry
 from geometry_msgs.msg import Point
-from tf.transformations import quaternion_from_euler
 
 def movebase_client():
 
     client = actionlib.SimpleActionClient('move_base',MoveBaseAction)
     client.wait_for_server()
+    while not rospy.is_shutdown():
+        rospy.loginfo("Esperando mensagem no /govai")
+        point = rospy.wait_for_message('/govai', Point)
+        # rospy.loginfo("Esperando odometria")
+        # odometry = rospy.wait_for_message('/odom', Odometry)
+        goal = MoveBaseGoal()
+        goal.target_pose.pose.position.x = point.x
+        goal.target_pose.pose.position.y = point.y
+        goal.target_pose.pose.orientation.w = 1
 
-    point = rospy.wait_for_message('/govai', Point)
-    goal = MoveBaseGoal()
-    goal.target_pose.header.frame_id = "map"
-    goal.target_pose.header.stamp = rospy.Time.now()
-    goal.target_pose.pose.position.x = point.x
-    goal.target_pose.pose.position.y = point.y
-    goal.target_pose.pose.orientation.w = 1
-    
-
-    client.send_goal(goal)
-    wait = client.wait_for_result()
-
-    goal = MoveBaseGoal()
-    goal.target_pose.header.frame_id = "base_link"
-    goal.target_pose.header.stamp = rospy.Time.now()
-    orientation = quaternion_from_euler(0,0,3.14)
-    rospy.loginfo(orientation)
-    goal.target_pose.pose.orientation.x = orientation[0]
-    goal.target_pose.pose.orientation.y = orientation[1]
-    goal.target_pose.pose.orientation.z = orientation[2]
-    goal.target_pose.pose.orientation.w = orientation[3]
-    client.send_goal(goal)
-    wait = client.wait_for_result()
-    if not wait:
-        rospy.logerr("Action server not available!")
-        rospy.signal_shutdown("Action server not available!")
-    else:
-        return client.get_result()
+        if point.z == 0:
+            rospy.loginfo("Movendo relativo")
+            goal.target_pose.header.frame_id = "base_footprint"
+            goal.target_pose.header.stamp = rospy.Time.now()
+            client.send_goal(goal)
+            wait = client.wait_for_result()
+        elif point.z == 1:
+            rospy.loginfo("Movendo absoluto")
+            goal.target_pose.header.frame_id = "map"
+            goal.target_pose.header.stamp = rospy.Time.now()
+            client.send_goal(goal)
+            wait = client.wait_for_result()
+            if wait and client.get_result():
+                rospy.loginfo("Goal execution done!")
+   
 
 if __name__ == '__main__':
     try:
         rospy.init_node('movebase_client_py')
-        result = movebase_client()
-        if result:
-            rospy.loginfo("Goal execution done!")
+        rospy.loginfo("move_base iniciado")
+        movebase_client()
     except rospy.ROSInterruptException:
         rospy.loginfo("Navigation test finished.")
