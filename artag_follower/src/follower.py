c762d59a5d3a3a9170239c9a5d1fa6f5403bad4c
VictorBelinello <victorbelinello17@gmail.com>
2021-05-03 19:46:28 -0300

    boa sorte

--- artag_follower/src/follower.py
+++ artag_follower/src/follower.py
@@ -2,20 +2,26 @@
 import rospy
 from ar_track_alvar_msgs.msg import AlvarMarkers
 from geometry_msgs.msg import Pose
+from std_srvs.srv import Empty
 
 target_id = 0
 pub = rospy.Publisher('/govai', Pose, queue_size=10)
 
 def update_goal(markers_data):
+    lost = False
     for m in markers_data.markers:
         if m.id == target_id:
             artag_pose = Pose()
             artag_pose.position.x = m.pose.pose.position.z - 0.8
             artag_pose.position.y = m.pose.pose.position.x
-
+            rospy.loginfo(m.pose.pose)
             if artag_pose.position.x > 0:
                 pub.publish(artag_pose)
-            
+
+    # rospy.wait_for_service('clear_costmaps')
+    # clear_costmaps = rospy.ServiceProxy('clear_costmaps', Empty)
+    # clear_costmaps()
+
 def get_artag_pose():
     rospy.init_node('follower', anonymous=True)
     rospy.Subscriber('/ar_pose_marker', AlvarMarkers, update_goal)
--- move_base_yolo/src/move_base.py
+++ move_base_yolo/src/move_base.py
@@ -4,10 +4,12 @@
 import rospy
 import actionlib
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
-from nav_msgs.msg import Odometry
+from std_msgs.msg import String
 from geometry_msgs.msg import Pose
 
 def movebase_client():
+    pub = rospy.Publisher('/lost', String, queue_size=1)
+    killer = rospy.Publisher('/killme', String, queue_size=1)
 
     client = actionlib.SimpleActionClient('move_base',MoveBaseAction)
     client.wait_for_server()
@@ -20,24 +22,36 @@ def movebase_client():
         goal = MoveBaseGoal()
         goal.target_pose.pose.position.x = pose.position.x
         goal.target_pose.pose.position.y = pose.position.y
+        goal.target_pose.pose.orientation.w = 1
+        
         
 
         if pose.position.z == 0:
             client.cancel_all_goals()
-            goal.target_pose.pose.orientation.w = 1
             rospy.loginfo("Movendo relativo")
             goal.target_pose.header.frame_id = "base_footprint"
             goal.target_pose.header.stamp = rospy.Time.now()
             client.send_goal(goal)
         elif pose.position.z == 1:
-            goal.target_pose.pose.orientation.w = 1
             rospy.loginfo("Movendo absoluto")
             goal.target_pose.header.frame_id = "map"
             goal.target_pose.header.stamp = rospy.Time.now()
             client.send_goal(goal)
             wait = client.wait_for_result()
             if wait and client.get_result():
+                msg = String()
+                msg.data = "0"
+                pub.publish(msg)
                 rospy.loginfo("Goal execution done!")
+        elif pose.position.z == -1:
+            rospy.loginfo("Movendo para base")
+            goal.target_pose.header.frame_id = "map"
+            goal.target_pose.header.stamp = rospy.Time.now()
+            client.send_goal(goal)
+            wait = client.wait_for_result()
+            if wait and client.get_result():
+                killer.publish(String())
+
    
 
 if __name__ == '__main__':
